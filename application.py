7791b1fa8c68d6afa41fc37fbed51819a9338f91
Nathan Ganser <47492950+nathanganser@users.noreply.github.com>
2020-09-25 17:54:36 +0200

    Update application.py

--- application.py
+++ application.py
@@ -2,12 +2,14 @@ from flask import Flask, jsonify
 import os
 from flask_sqlalchemy import SQLAlchemy
 from waitress import serve
+from dotenv import load_dotenv
+from models import GoogleAPI
 
-
-application = Flask(__name__)
-application.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE']
-application.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
-db = SQLAlchemy(application)
+load_dotenv()
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+db = SQLAlchemy(app)
 
 
 class Error(Exception):
@@ -15,33 +17,16 @@ class Error(Exception):
         self.error = error
         self.status_code = status_code
 
-# Database models
-class Table(db.Model):
-    __tablename__ = 'table'
-    id = db.Column(db.Integer, primary_key=True)
-    string = db.Column(db.String)
-    datetime = db.Column(db.DateTime)
-    integer = db.Column(db.Integer)
-
-    def insert(self):
-        db.session.add(self)
-        db.session.commit()
-
-    def delete(self):
-        db.session.delete(self)
-        db.session.commit()
-
-    def update(self):
-        db.session.commit()
-
 
 # URLs
-@application.route('/')
+@app.route('/')
 def index():
     return "success"
 
 
-@application.errorhandler(Error)
+
+
+@app.errorhandler(Error)
 def error(error):
     return jsonify({
         "success": False,
@@ -51,5 +36,5 @@ def error(error):
 
 
 if __name__ == '__main__':
-    application.run(host='0.0.0.0', port=8080, debug=True)
-    # serve(application, port=80)
+    app.run(host='0.0.0.0', port=8080, debug=True)
+    # serve(app, port=80)
