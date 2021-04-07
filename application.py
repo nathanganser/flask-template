f140caceca2af3c01279fa88ec11a9b9ccb9790d
Nathan Ganser <>
2021-04-07 09:19:15 +0000

    fin première base de données

--- application.py
+++ application.py
@@ -1,15 +1,13 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import os
 from flask_sqlalchemy import SQLAlchemy
 from waitress import serve
 from dotenv import load_dotenv
-from models import Contacts
+from models import Contacts, app
 
 load_dotenv()
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
-db = SQLAlchemy(app)
+
+
 
 
 class Error(Exception):
@@ -27,7 +25,31 @@ def index():
 @app.route('/contacts', methods=['GET', 'POST'])
 def contacts():
   contacts = Contacts.query.all()
-  return jsonify(contacts)
+  json = {}
+  for contact in contacts:
+    json[contact.id] = contact.nom
+
+  return jsonify(json)
+
+@app.route('/contact/<id>')
+def contact(id):
+  contact = Contacts.query.filter_by(id=id).first()
+  return jsonify(id=contact.id, nom=contact.nom)
+
+@app.route('/contact', methods=['POST', 'DELETE'])
+def create_or_delete_contact():
+  data = request.json
+  nom = data.get('nom')
+
+  if request.method == 'POST':
+    
+    contact = Contacts(nom=nom)
+    contact.insert()
+    return jsonify(message="Contact créé!")
+  elif request.method == 'DELETE':
+    contact = Contacts.query.filter_by(nom=nom).first()
+    contact.delete()
+    return jsonify(message="Contact supprimé")
 
 
 
