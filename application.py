3e6eb25aad8bff1fdbb131bbf7a078815747253b
Nathan Ganser <me@nathanganser.com>
2021-04-07 11:43:31 +0200

    clean up

--- application.py
+++ application.py
@@ -8,7 +8,6 @@ from models import Contacts, app
 load_dotenv()
 
 
-
 class Error(Exception):
     def __init__(self, error, status_code):
         self.error = error
@@ -18,39 +17,40 @@ class Error(Exception):
 # URLs
 @app.route('/')
 def index():
-  age = 25
-  return f"Ceci est le premier serveur de Nathan, qui a {age} ans"
+    age = 25
+    return f"Ceci est le premier serveur de Nathan, qui a {age} ans"
+
 
 @app.route('/contacts', methods=['GET', 'POST'])
 def contacts():
-  contacts = Contacts.query.all()
-  json = {}
-  for contact in contacts:
-    json[contact.id] = contact.nom
+    contacts = Contacts.query.all()
+    json = {}
+    for contact in contacts:
+        json[contact.id] = contact.nom
 
-  return jsonify(json)
+    return jsonify(json)
+
 
 @app.route('/contact/<id>')
 def contact(id):
-  contact = Contacts.query.filter_by(id=id).first()
-  return jsonify(id=contact.id, nom=contact.nom)
+    contact = Contacts.query.filter_by(id=id).first()
+    return jsonify(id=contact.id, nom=contact.nom)
+
 
 @app.route('/contact', methods=['POST', 'DELETE'])
 def create_or_delete_contact():
-  data = request.json
-  nom = data.get('nom')
+    data = request.json
+    nom = data.get('nom')
 
-  if request.method == 'POST':
-    
-    contact = Contacts(nom=nom)
-    contact.insert()
-    return jsonify(message="Contact créé!")
-  elif request.method == 'DELETE':
-    contact = Contacts.query.filter_by(nom=nom).first()
-    contact.delete()
-    return jsonify(message="Contact supprimé")
+    if request.method == 'POST':
 
-
+        contact = Contacts(nom=nom)
+        contact.insert()
+        return jsonify(message="Contact créé!")
+    elif request.method == 'DELETE':
+        contact = Contacts.query.filter_by(nom=nom).first()
+        contact.delete()
+        return jsonify(message="Contact supprimé")
 
 
 @app.errorhandler(Error)
--- models.py
+++ models.py
@@ -6,25 +6,23 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 
-
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE']
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+
 class Contacts(db.Model):
-  __tablename__= "contacts"
-  id = db.Column(db.Integer, primary_key=True)
-  nom = db.Column(db.String)
+    __tablename__ = "contacts"
+    id = db.Column(db.Integer, primary_key=True)
+    nom = db.Column(db.String)
 
-  def insert(self):
-    db.session.add(self)
-    db.session.commit()
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
 
-  def delete(self):
-    db.session.delete(self)
-    db.session.commit()
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
 
-  def update(self):
-    db.session.commit()
-
-
+    def update(self):
+        db.session.commit()
